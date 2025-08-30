e5f7d1c1d38df1067fd9f7b297774b2ead4a7d16
Gongora-G <jhoangongora71@gmail.com>
2025-08-30 13:48:05 -0500

    se elimino el hola

--- saludo.py
+++ saludo.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-﻿print("geste es el laboratorio inicial del proyecto de administración de caddies!")
-=======
-﻿print("¡Hola, este es el laboratorio inicial del proyecto de administración de caddies!")
->>>>>>> 3421a7b4d9a19b538ee09cd193e4741c659dc1b2
+print("Este es el laboratorio inicial del proyecto de administración de caddies!")
